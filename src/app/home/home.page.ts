c955fa1eb40284592844a92ff10072ff92fde49f
DamnCode <davidfrancisco881@gmail.com>
2024-12-05 21:15:48 +0700

    add project

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  cryptoList: any[] = []; // Menyimpan daftar cryptocurrency
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
+  // Fungsi untuk mengambil data dari API
+  getCryptoData() {
+    const apiUrl = 'https://api.coinlore.net/api/tickers/';
+
+    this.http.get(apiUrl).subscribe((response: any) => {
+      this.cryptoList = response.data.map((item: any) => ({
+        rank: item.rank,
+        name: item.name,
+        symbol: item.symbol,
+        price_usd: parseFloat(item.price_usd).toFixed(2), // Format harga menjadi dua desimal
+      }));
+    });
+  }
 }
